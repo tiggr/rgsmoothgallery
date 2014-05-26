9137ac9eda2af5395d255455891f073ec881d64f
Daniel Schöne <daniel@schoene.it>
2014-05-26 17:04:49 +0200

    initial import (v1.5.2 from TER)

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "rgsmoothgallery".
+ *
+ * Auto generated 13-05-2013 19:12
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'SmoothGallery for TYPO3',
+	'description' => 'Slideshow & Gallery. Shows images from directory folders, from records and inside tt_content (Element "Text with images") DAM and tt_news. +Thumbnail-Browser, +Lightbox. Demo: http://www.rggooglemap.com/dev/rgsmoothgallery.html',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.5.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => 'uploads/tx_rgsmoothgallery/',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Georg Ringer (just2b)',
+	'author_email' => 'http://www.just2b.com',
+	'author_company' => 'http://www.just2b.com',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
