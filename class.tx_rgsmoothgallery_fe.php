b0d350b44aa4dde66b61f7316c923819b969b204
Daniel Schöne <daniel@schoene.it>
2014-09-08 15:17:54 +0200

    adds german & danish more pics string

--- class.tx_rgsmoothgallery_fe.php
+++ class.tx_rgsmoothgallery_fe.php
@@ -28,90 +28,94 @@
 */
 
 class tx_rgsmoothgallery_fe{
-	
+
 	// hook for tt_news
 	function extraItemMarkerProcessor($markerArray, $row, $lConf, &$pObj) {
-		$this->cObj = t3lib_div::makeInstance('tslib_cObj'); // local cObj.	
+		$this->cObj = t3lib_div::makeInstance('tslib_cObj'); // local cObj.
 		$this->pObj = &$pObj;
 		$this->realConf = $pObj;
-		
+
 		// configuration array of rgSmoothGallery
 		$rgsgConfDefault = $this->realConf->conf['rgsmoothgallery.'];
-		
+
 		// merge with special configuration (based on chosen CODE [SINGLE, LIST, LATEST]) if this is available
 		if (is_array($rgsgConfDefault[$pObj->config['code'].'.'])) {
 			$rgsgConf = t3lib_div::array_merge_recursive_overrule($rgsgConfDefault, $rgsgConfDefault[$pObj->config['code'].'.']);
 		} else {
 			$rgsgConf = $rgsgConfDefault;
 		}
-		
+
 		#echo t3lib_utility_Debug::viewArray($rgsgConf);
 		$this->rgsgConf = $rgsgConf;
 		// if the configuration is available, otherwise just do nothing
 		if ($rgsgConf) {
-		
+
 		// unique ID > uid of the record
 		$uniqueId = $row['uid'];
-		
+
 		// possibility to use a different field for the images + caption
 		$imageField = $this->rgsgConf['imageField'] ? $this->rgsgConf['imageField'] : 'image';
 		$imageFieldPrefix = $this->rgsgConf['imageFieldPrefix'] ? $this->rgsgConf['imageFieldPrefix'] : 'uploads/pics/';
 		$captionField = $this->rgsgConf['captionField'] ? $this->rgsgConf['captionField'] : 'imagecaption';
-		
-		
+
+
 		// query for the images & caption
 		$field = 'pid,uid,'.$imageField.','.$captionField;
 		$table = 'tt_news';
-		$where = 'uid = '.$uniqueId;   
+		$where = 'uid = '.$uniqueId;
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($field,$table,$where);
 		$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
 		if ($GLOBALS['TSFE']->sys_language_content) {
 			$OLmode = ($this->sys_language_mode == 'strict'?'hideNonTranslated':'');
 			$row = $GLOBALS['TSFE']->sys_page->getRecordOverlay('tt_news', $row, $GLOBALS['TSFE']->sys_language_content, '');
 		}
-		
+
 		// needed fields: image & imagecaption
 		$images = explode(',',$row[$imageField]);
 		$caption = explode("\n",$row[$captionField]);
-		
+
 		// If there are any images and minimum count of images is reached
 		if ($row[$imageField] && count($images) >= $rgsgConf['minimumImages']) {
 			// call rgsmoothgallery
-			require_once( t3lib_extMgm::extPath('rgsmoothgallery').'pi1/class.tx_rgsmoothgallery_pi1.php'); 
+			require_once( t3lib_extMgm::extPath('rgsmoothgallery').'pi1/class.tx_rgsmoothgallery_pi1.php');
 			$this->gallery = t3lib_div::makeInstance('tx_rgsmoothgallery_pi1');
-			
+
 			// if no js is available
 			$noJsImg =   $rgsgConf['big.'];
-			$noJsImg['file'] = $imageFieldPrefix.$images[0];   
-			
+			$noJsImg['file'] = $imageFieldPrefix.$images[0];
+
 			if ($rgsgConf['externalControl']==1) {
 				$externalControl1 = 'var myGallery'.$uniqueId.';';
 			} else {
-				$externalControl2 = 'var';  
+				$externalControl2 = 'var';
 			}
-			
+
 			// real unique key, needed for more than 1 view of tt_news on 1 page
-			$uniqueId = $this->realConf->config['code'].$uniqueId;    
-			
+			$uniqueId = $this->realConf->config['code'].$uniqueId;
+
 			// configuration of gallery
 			$lightbox = ($rgsgConf['lightbox']==1) ? 'true' : 'false';
 			$lightbox2= ($rgsgConf['lightbox']==1) ? 'var mylightbox = new LightboxSmoothgallery();' : '';
 			$duration = ($rgsgConf['duration']) ? 'timed:true,delay: '.$rgsgConf['duration'] : 'timed:false';
 			$thumbs   = ($rgsgConf['showThumbs']==1) ? 'true' : 'false';
 			$arrows   = ($rgsgConf['arrows']==1) ? 'true' : 'false';
-			
+
 			// advanced settings (from TS + tab flexform configuration)
-			$advancedSettings.=  ($rgsgConf['hideInfoPane']==1) ? 'showInfopane: false,' : ''; 
+			$advancedSettings.=  ($rgsgConf['hideInfoPane']==1) ? 'showInfopane: false,' : '';
 			if ($rgsgConf['thumbOpacity'] && $rgsgConf['thumbOpacity'] > 0 && $rgsgConf['thumbOpacity']<=1) $advancedSettings.= 'thumbOpacity: '.$rgsgConf['thumbOpacity'].',';
-			if ($rgsgConf['slideInfoZoneOpacity'] && $rgsgConf['slideInfoZoneOpacity'] && $rgsgConf['slideInfoZoneOpacity'] > 0 && $rgsgConf['slideInfoZoneOpacity']<=1) $advancedSettings.= 'slideInfoZoneOpacity: '.$rgsgConf['slideInfoZoneOpacity'].',';   
-			$advancedSettings.=  ($rgsgConf['thumbSpacing']) ? 'thumbSpacing: '.$rgsgConf['thumbSpacing'].',' : ''; 
-			
+			if ($rgsgConf['slideInfoZoneOpacity'] && $rgsgConf['slideInfoZoneOpacity'] && $rgsgConf['slideInfoZoneOpacity'] > 0 && $rgsgConf['slideInfoZoneOpacity']<=1) $advancedSettings.= 'slideInfoZoneOpacity: '.$rgsgConf['slideInfoZoneOpacity'].',';
+			$advancedSettings.=  ($rgsgConf['thumbSpacing']) ? 'thumbSpacing: '.$rgsgConf['thumbSpacing'].',' : '';
+
 			// external thumbs
 			$advancedSettings.= ($rgsgConf['externalThumbs']) ? 'useExternalCarousel:true,carouselElement:$("'.$rgsgConf['externalThumbs'].'"),' : '';
-			
-			
+
+			$morePicsString = 'weitere Bilder';
+			if ($GLOBALS['TSFE']->sys_language_uid == 1) {
+				$morePicsString = 'flere billeder';
+			}
+
 			// configuration
-			$configuration = '		
+			$configuration = '
 				<script type="text/javascript">'.$externalControl1.'
 					function startGallery'.$uniqueId.'() {
 						if(window.gallery'.$uniqueId.') {
@@ -120,9 +124,10 @@ class tx_rgsmoothgallery_fe{
 									'.$duration.',
 									showArrows: '.$arrows.',
 									showCarousel: '.$thumbs.',
+									textShowCarousel: "'.$morePicsString.'",
 									embedLinks:'.$lightbox.',
 									'.$advancedSettings.'
-									lightbox:true    					    
+									lightbox:true
 								});
 								var mylightbox = new LightboxSmoothgallery();
 							}	catch(error){
@@ -142,11 +147,11 @@ class tx_rgsmoothgallery_fe{
 				<noscript>
 					<div><img src="'.$this->cObj->IMG_RESOURCE($noJsImg).'"  /></div>
 				</noscript>
-			';       
-			
+			';
+
 			// get the JS
-			
-			
+
+
 			$content =$this->gallery->getJs(1,1,1,0,$rgsgConf['width'],$rgsgConf['height'],$rgsgConf['width'],$rgsgConf['height'],'',$uniqueId,$rgsgConf,$configuration);
 			// Begin the gallery
 			$content.=  $this->gallery->beginGallery($uniqueId);
@@ -158,19 +163,19 @@ class tx_rgsmoothgallery_fe{
 				$imgTSConfigThumb = $rgsgConf['thumb.'];
 				$imgTSConfigThumb['file'] = $path;
 				$imgTSConfigBig =   $rgsgConf['big.'];
-				$imgTSConfigBig['file'] = $path;        
+				$imgTSConfigBig['file'] = $path;
 				$imgTSConfigLightbox = $rgsgConf['lightbox.'];
-				$imgTSConfigLightbox['file'] = $path;        
+				$imgTSConfigLightbox['file'] = $path;
 				# $lightbox = ($rgsgConf['lightbox']==1) ? $this->cObj->IMG_RESOURCE($imgTSConfigLightbox) : $this->cObj->IMG_RESOURCE($imgTSConfigLightbox);
-				
+
 				// caption text
 				$text =explode('|',$caption[$i]);
-				
+
 				// add image
-				
+
 				$content.=$this->addImage(
 					$path,
-					$text[0], 
+					$text[0],
 					$text[1],
 					true,
 					true,
@@ -179,58 +184,58 @@ class tx_rgsmoothgallery_fe{
 				);
 				$i++;
 			} # end foreach file
-			
-			// end of image    	 
-			$content.=$this->gallery->endGallery(); 
-			
-			// write new gallery into the marker    
+
+			// end of image
+			$content.=$this->gallery->endGallery();
+
+			// write new gallery into the marker
 			$markerName = $this->rgsgConf['imageMarker'] ? $this->rgsgConf['imageMarker'] : 'NEWS_IMAGE';
 			$markerArray['###'.$markerName.'###'] ='<div class="news-single-img">'.$content.'</div>';
 			}  elseif ($this->rgsgConf['imageMarker']!='') {
 				$markerArray['###'.$this->rgsgConf['imageMarker'].'###'] = '';
 			}
 		} # end if ($rgsgConf) {
-		
+
 		return $markerArray;
 	} #end extraItemMarkerProcessor
-	
+
 	function addImage($path,$title,$description,$thumb,$lightbox,$uniqueID,$limitImages=0) {
 		if ($this->rgsgConf['hideInfoPane']!=1) {
 			$text = (!$title) ? '' : "<h3>$title</h3>";
 			$text.=(!$description) ? '' : "<p>$description</p>";
 		}
-		
+
 		//  generate images
 		if ($this->rgsgConf['watermark']) {
 			$imgTSConfigBig = $this->rgsgConf['big2.'];
 			$imgTSConfigBig['file.']['10.']['file'] = $path;
 			$imgTSConfigLightbox = $this->rgsgConf['lightbox2.'];
-			$imgTSConfigLightbox['file.']['10.']['file'] = $path; 
+			$imgTSConfigLightbox['file.']['10.']['file'] = $path;
 		} else {
 			$imgTSConfigBig = $this->rgsgConf['big.'];
 			$imgTSConfigBig['file'] = $path;
 			$imgTSConfigLightbox = $this->rgsgConf['lightbox.'];
-			$imgTSConfigLightbox['file'] = $path;               
-		}  
+			$imgTSConfigLightbox['file'] = $path;
+		}
 		$bigImage = $this->cObj->IMG_RESOURCE($imgTSConfigBig);
-		
+
 		$lightbox =  ($this->rgsgConf['lightbox']) ? $this->cObj->IMG_RESOURCE($imgTSConfigLightbox) : 'javascript:void(0)' ;
 		$lightBoxImage='<a href="'.$lightbox.'" title="Open Image" class="open"></a>';
-		
+
 		if ($this->rgsgConf['showThumbs']) {
 			$imgTSConfigThumb = $this->rgsgConf['thumb.'];
-			$imgTSConfigThumb['file'] = $path;     
+			$imgTSConfigThumb['file'] = $path;
 			$thumbImage = '<img src="'.$this->cObj->IMG_RESOURCE($imgTSConfigThumb).'" class="thumbnail" />';
 		}
-		
-		// build the image element    
+
+		// build the image element
 		$singleImage .= '
 			<div class="imageElement">
 			'.$text.$lightBoxImage.'
 			<img src="'.$bigImage.'" class="full" />
 			'.$thumbImage.'
-			</div>';    
-			  
+			</div>';
+
 		return $singleImage;
 	}
 
