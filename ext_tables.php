a930e25df4977ec620f15d42ecefab3fe302e759
Daniel Schöne <daniel@schoene.it>
2015-02-11 23:30:55 +0100

    Removes call to loadTCA

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages';
 
 
